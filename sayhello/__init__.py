adfa56ac0d051663290be3a27a9335d81dbb5be3
Xingyu Guo <1344166268@qq.com>
2019-05-27 16:26:57 +0800

    更新

--- sayhello/__init__.py
+++ sayhello/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, get_debug_queries
 
 app = Flask('sayhello')
 app.jinja_env.trim_blocks = True
@@ -20,3 +20,11 @@ from sayhello import views, commands, errors  # noqa
 @app.shell_context_processor
 def make_shell_context():
     return dict(db=db, bootstrap=bootstrap)
+
+#
+# @app.after_request
+# def query_profiler(response):
+#     for q in get_debug_queries():
+#         print('%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n' % (q.statement, q.parameters, q.start_time, q.end_time, q.duration, q.context))
+#         print('-' * 120)
+#     return response
